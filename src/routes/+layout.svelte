1eb24b50ed3df7eacb1d22422b6b1fdc120c1535
XerAnime <160598198+XerAnime@users.noreply.github.com>
2024-04-12 12:07:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import Header from './Components/Header.svelte';
+	export let data;
+	let headerData = {
+		siteName: data.siteName,
+		navLinks: [{ text: 'Cool site for dramas', url: 'https://dramacool.pa' }]
+	};
+</script>
+
+<div class="container bg-sky-300 p-7">
+	<Header {headerData} />
+	<main>
+		<slot />
+	</main>
+</div>
---
+++ src/routes/Components/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let headerData;
+	import { Search } from 'lucide-svelte';
+</script>
+
+<header>
+	<div class="container flex justify-between font-serif px-4 text-3xl">
+		<h1>{headerData.siteName}</h1>
+		<nav>
+			<ul>
+				{#each headerData.navLinks as link}
+					<li>
+						<a href={link.url}
+							><span class="text-white italic md:tracking-[.30em]">{link.text}</span>
+						</a>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+
+		<form>
+			<div class="relative flex flex-col items-end">
+				<input class="px-4 py-1 text-base" type="text" id="search" placeholder="Search" />
+
+				<button class="absolute bg-orange-300 p-1">
+					<Search />
+				</button>
+			</div>
+		</form>
+	</div>
+</header>
